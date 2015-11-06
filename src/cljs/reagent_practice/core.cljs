c2a9453851a70b1e6ca9b8571f993e4bf49af76c
Manish <manishkumar.singh103@gmail.com>
2015-11-06 20:15:33 +0530

    deleting

--- src/cljs/reagent_practice/core.cljs
+++ src/cljs/reagent_practice/core.cljs
@@ -9,12 +9,14 @@
 
 ;; Helpers
 ;; =======
-(defn drop-nth-note [coll n]
-  (let [[a b] (split-at n coll)]
-    (vec (concat a (rest b)))))
-
 (defn get-value-by-id [id]
   (.-value (.getElementById js/document id)))
+
+(defn delete-note-by-id! [id]
+  (swap! notes
+         #(vec (filter (fn [note]
+                         (not= id (:id note)))
+                       %))))
 
 (defn save-note! []
   (let [note-val (get-value-by-id "note")]
@@ -27,14 +29,18 @@
                      (.log js/console data)
                      (swap! notes conj note)))})))
 
-(defn delete-note! [id])
+(defn delete-note! [id]
+  (GET "/delete-note"
+       {:params  {:id id}
+        :handler (fn [response]
+                   (delete-note-by-id! id))}))
 
 (defn home-page []
   [:div
    [:h "Todo List"]
    [:ul {:style {:font-size "14px"}}
     (for [{:keys [id val]} @notes]
-      [:li {:onClick #(delete-note! id)}
+      [:li {:onClick #(delete-note-by-id! id)}
         val])]
    [:input#note {:style {:margin-right "2px"}}]
    [:button {:onClick save-note!}
